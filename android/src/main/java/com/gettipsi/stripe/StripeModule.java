62115620f0bbd27850f33f0fa3c9e055f064f97e
Jim Lake <jim@blueskylabs.com>
2019-07-21 15:41:06 -0700

    nullable to javax

--- android/src/main/java/com/gettipsi/stripe/StripeModule.java
+++ android/src/main/java/com/gettipsi/stripe/StripeModule.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import javax.annotation.Nullable;
 import android.text.TextUtils;
 
 import com.facebook.react.bridge.ActivityEventListener;
